fd9fb685df8685e25199f8c196bfe407ed250e8b
shamama <rohishamama@gmail.com>
2022-09-23 11:35:56 -0700

    protofolioweb

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Navbar Sticky
+window.addEventListener("scroll", (e) => {
+    const navbar = document.querySelector("#navbar");
+        navbar.classList.toggle('sticky', window.scrollY > 150);
+})
+
+
+// Navbar Control
+const burger = document.querySelector(".burger");
+const close = document.querySelector(".close");
+burger.addEventListener("click", () => {
+    const mainMenu = document.querySelector(".mainMenu");
+    const item = document.querySelectorAll(".mainMenu ul a")
+    item.forEach(element => {
+        setTimeout(() => {
+            element.classList.add("smooth")
+        }, 200);
+        })
+    mainMenu.classList.add("active")
+})
+close.addEventListener("click", () => {
+    const mainMenu = document.querySelector(".mainMenu");
+    const item = document.querySelectorAll(".mainMenu ul a")
+    item.forEach(element => {
+        element.classList.remove("smooth")
+    })
+    mainMenu.classList.remove("active")
+})
